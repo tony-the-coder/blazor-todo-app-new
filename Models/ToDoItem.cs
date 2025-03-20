b5fec712d59b431224dfa74a2f8d3cb0ab968a63
Tony Aldea <tonythecoder@outlook.com>
2025-03-20 11:41:01 -0400

    Reinitialize Git repository and add all files

---
+++ Models/ToDoItem.cs
@@ -0,0 +1,13 @@
+﻿namespace ToDoApp.Models
+{
+    public class ToDoItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = "";
+        public bool IsCompleted { get; set; }
+
+        public DateTime DateCompleted { get; set; }
+    }
+}
+
+//THIS IS THE END OF THE SNIPPET
